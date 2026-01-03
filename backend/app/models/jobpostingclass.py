65b71222897aa5fce4192997b4ec7cdbb18650bd
KC2033 <dragonitezball@gmail.com>
2026-01-03 21:52:33 +0000

    Added getter methods to JobPostingClass, removed unused test data files

--- backend/app/models/jobpostingclass.py
+++ backend/app/models/jobpostingclass.py
@@ -6,3 +6,15 @@ class JobPosting:
         self.job_desc = job_desc
         self.application_link = application_link
         self.cv_similarity_score = cv_similarity_score
+    
+    def get_job_name(self):
+        return self.job_name
+    
+    def get_job_description(self):
+        return self.job_desc
+    
+    def get_application_link(self):
+        return self.application_link
+    
+    def get_similarity_score(self):
+        return self.cv_similarity_score
